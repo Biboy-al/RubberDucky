3daebf41a2d702b47b6423341670362753a5f2e7
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-09-05 00:13:28 +1200

    feat: api calls starts a job to glacia

--- rubber-ducky/lib/transcript.ts
+++ rubber-ducky/lib/transcript.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Form } from "react-hook-form";
+import { start } from "repl";
 
 async function urlToBlob(url: string): Promise<Blob> {
     const response = await fetch(url);
@@ -7,18 +8,31 @@ async function urlToBlob(url: string): Promise<Blob> {
 }
 
 async function blobUrlToFile(blobUrl: string, fileName:string){
+
     const blob = await urlToBlob(blobUrl);
     return new File([blob], fileName, {type:blob.type || 'audio/wav'})
 }
 
-
 export default function Transcribe() {
-    const [script, setScript] = useState<string>("");
+
+    const transctibeAudio = async (blobUrl: string) =>{
+
+        const audio_data = await uploadAudio(blobUrl,"https://api.gladia.io/v2/upload");
+        const audio_json = await audio_data.json();
+
+        const trans_job_data = await startTranscription(audio_json.audio_url, "https://api.gladia.io/v2/pre-recorded");
+        const trans_job_json = await trans_job_data.json();
+
+        const trans_data = await getTranscript(trans_job_json.result_url, "https://api.gladia.io/v2/pre-recorded");
+        const trans_json = await trans_data.json();
+
+        return trans_json.result.transcript
+
+    }
 
     const uploadAudio = async (blobUrl: string,endpoint: string) =>{
         const file = await blobUrlToFile(blobUrl, "audio.wav");
         const formData = new FormData();
-        formData
         formData.append('audio', file);
     
         const response = await fetch(endpoint, {
@@ -29,10 +43,42 @@ export default function Transcribe() {
             body: formData
         });
 
-        const data = await response.json();
+        return response;
 
-        console.log(data.audio_url)
     }
+
+
+    const startTranscription = async (audioUrl: string, endpoint: string) =>{
+
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-gladia-key': process.env.NEXT_PUBLIC_GLADIA_API_KEY || '',
+            },
+            body: JSON.stringify({
+                audio_url: audioUrl
+            })
+        });
+
+        return response;
+
+    }
+
+    const getTranscript = async(transUrl: string, endpoint: string) =>{
+
+        const params = new URLSearchParams();
+        params.append("id", transUrl);
+
+        const response = await fetch(`${endpoint}${params}`, {
+            headers: {
+                'x-gladia-key': process.env.NEXT_PUBLIC_GLADIA_API_KEY || '',
+            },
+        });
+
+        return response;
+    }
+
 
     return {uploadAudio};
 }
